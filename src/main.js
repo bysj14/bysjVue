747317f7a14f255c986a27ea17b30602aba22280
stgljy <stg761210>
2017-12-25 15:36:35 +0800

    test2

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App}//组件
 })
+
+
